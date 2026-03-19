8f5f601d3f440d0782cc48da27d4224bde1303da
KIMHYEOK_GAMING\borad <kimmang940715@naver.com>
2026-03-20 00:54:39 +0900

    [초보자를 위한 C++ 200제] 입문 003번

---
+++ ConsoleApplication1/ConsoleApplication1/CoutCinEndl.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main_01()
+{
+    int number = 0;
+
+    cout << " 숫자를 입력하세요";
+
+    cin >> number;
+
+    cout << "입력한 숫자는 " << number << "입니다." << endl;
+
+    return 0;
+}
+
